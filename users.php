147ab4b146a9021df3644998018f15710909af5c
CL4WED <claude.ebnol@gmail.com>
2026-09-23 22:16:41 +0800

    Add files via upload

---
+++ users.php
@@ -0,0 +1,21 @@
+<h1>User Accounts</h1>
+
+<a href="/">Home</a> |
+<a href="/about">About</a> |
+<a href="/customers">Customer Accounts</a> |
+<a href="/users">User Accounts</a>
+
+<table border="1" cellpadding="8">
+    <tr>
+        <th>Username</th>
+        <th>Full Name</th>
+        <th>Role</th>
+    </tr>
+
+    <?php foreach ($users as $user): ?>
+        <tr>
+            <td><?= esc($user['username']) ?></td>
+            <td><?= esc($user['full_name']) ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
